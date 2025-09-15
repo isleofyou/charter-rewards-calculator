983306956aca717d1b88378789b0968e8cb439e7
David Tran <frozenpho@gmail.com>
2025-09-15 11:09:29 -0600

    Update App.js to use custom component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import RewardsCalculator from './components/RewardsCalculator';
 
 function App() {
   return (
-    <div>Hello World</div>
+    <div className="App">
+      <header className="App-header">
+        <h1>🦁 Charter Wild Kingdom Pet Supplies 🐘</h1>
+        <h2>Customer Rewards Program</h2>
+      </header>
+      <main>
+        <RewardsCalculator />
+      </main>
+    </div>
   );
 }
 
